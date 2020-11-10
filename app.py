8d04251ff267813e3288b001a8bf87e9ede58964
Rax <damnloardbruh@gmail.com>
2020-11-10 20:03:35 +0700

    push loop

--- app.py
+++ app.py
@@ -16,19 +16,78 @@ majors = {
     } 
 q1 = 'do you like computer?'
 a1 = {'1': ['cs','des'],'2': ['chef','law','hnt']}
-user_input  = input("please enter 1/2")
-major_list = []
+q2 = 'I\'m interested in law, debate, government, and politics.'
+q3 = "I'm interested in intellectual ideas, including those that are philosophical or religious."
+q4 = "I enjoy performing as an actor, or working behind the scenes on theatrical productions."
+q5 = "I like to offer advice to others."
+q6 = "I'm concerned about the state of the environment and want to work to improve it."
+q7 = "I enjoy working with people, and I have strong verbal and written communication skills."
+q8 = "I enjoy learning about how the human body and natural world work."
+q9 = "I want to work with people on the margins of society or who have been oppressed."
+q10 = "I'm interested in graphic and/or Web design."
+q11 = "I'm very ambitious, persuasive, and love coming up with my own ideas."
+q12 = "I especially love working with children."
+q13 = "I'm interested in science and enjoy helping people."
+q14 = "I like to sing and/or play musical instruments."
+q15 = "I have a strong interest and ability in visual art."
+q16 = "I enjoy learning about different parts of the world."
+q17 = "I like to work with my hands and be outdoors."
+q18 = "I enjoy learning about languages."
+q19 = "I need the freedom to be creative and express myself."
+q20 = "I'm good with numbers and am detail-oriented."
+q21 = "I enjoy helping people."
+q22 = "I am interested in helping bodies heal and rehabilitate."
+q23 = "I like math and figuring out how things work."
+q24 = "I'm interested in technology and learning how computers work."
+q25 = "I am interested in conflict resolution, criminal justice or mediation."
+q26 = "I am always reading a book or writing my own stories."
+q27 = "I like to experiment with better and faster ways of doing things."
+
+
 # tes = ['cs','des']
 # major_list.append(tes)
+# print(major_list)
+# print(type(major_list))
+questions = {
+    q1:a1,
+    q2:'2',
+    q3:'3',
+    q4:'3',
+    q5:'3',
+    q6:'3',
+    q7:'3',
+    q8:'3',
+    q9:'3',
+    q10:'3',
+    q11:'3',
+    q12:'3',
+    q13:'3',
+    q14:'3',
+    q15:'3',
+    q16:'3',
+    q17:'3',
+    q18:'3',
+    q19:'3',
+    q20:'3',
+    q21:'3',
+    q22:'3',
+    q23:'3',
+    q24:'3',
+    q25:'3',
+    q26:'3',
+    q27:'3',
+    }
+
+
+    # print(ans)
+    # print(type(ans))
+for i in questions:
+    # user_input  = input("please enter 1 or 2 : ")
+    # if user_input:
+    major_list = []
+    #     ans = questions[i]
+    #     major_list.append(ans)
+    # return 0
 print(major_list)
-print(type(major_list))
 
-if user_input:
-    ans = a1[user_input]
-    major_list.append(ans)
-
-    print(major_list)
-    print(ans)
-    print(type(ans))
-
-print (q1)
+# print (q1)
